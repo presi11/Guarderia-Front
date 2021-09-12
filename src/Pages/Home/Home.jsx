29d54ee53357f0f47f93a84356e8a89de83ad692
presi11 <44074556+presi11@users.noreply.github.com>
2021-09-12 17:16:16 -0500

    Revert "Change home"

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,10 +11,10 @@ return(
     <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
       <div className='d-flex justify-content-center align-items-center h-100'>
         <div className='text-white'>
-          <h1 className='mb-3'>Inico</h1>
-          <h4 className='mb-3'>Pexshop</h4>
+          <h1 className='mb-3'>Heading</h1>
+          <h4 className='mb-3'>Subheading</h4>
           <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-            Mascotas
+            Call to action
           </a>
         </div>
       </div>

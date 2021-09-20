cdb83e6f74542a1ed449590fc40b1bf7715d9d62
Esteban Ospina <juan.ospina19@udea.edu.co>
2021-09-20 12:50:48 -0500

    upgrade form pet

--- src/Pages/Pets/RegisterPet.jsx
+++ src/Pages/Pets/RegisterPet.jsx
@@ -1,118 +1,142 @@
 import React, { useState } from "react";
 import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import { registerPet } from "../../services/registerPet";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+import CompleteFormPet from "../../Component/Modalforms/CompleteFormPet";
 
 const Register = () => {
+  
   const [formState, setFormState] = useState({
     petName: "",
     raceId: "",
-    ownerId:1,
+    ownerId: 1,
     size: "",
     age: "",
     vaccinationPlan: "",
     careToHave: "",
   });
 
-  function sendForm(){
+  function sendForm() {
     registerPet(formState);
     console.log(formState);
   }
-  
+  const mystyle = {
+    width: "23rem",
+  };
 
+  const [gridModal, setGridModal] = useState(false);
   return (
-    <div style={{ width: "23rem" }}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          sendForm();
-        }}
-      >
-        <br/>
-        <MDBInput
-          value={formState.petName}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              petName: e.target.value,
-            })
-          }
-          label="Nombre de la mascota"
-          id="formControlDefault"
-          type="text"
-        />
+    <section class="border d-flex justify-content-center p-4 mb-4">
+      <div style={mystyle}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendForm();
+            setGridModal(!gridModal);
+          }}
+        >
+          <br />
+          <MDBInput
+            value={formState.petName}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                petName: e.target.value,
+              })
+            }
+            label="Nombre de la mascota"
+            id="formControlDefault"
+            type="text"
+          />
 
-        <br />
-        <MDBInput
-          value={formState.raceId}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              raceId: e.target.value,
-            })
-          }
-          label="Raza"
-          id="formControlDefault"
-          type="text"
-        />
+          <br />
 
-        <br />
-        <MDBInput
-          value={formState.size}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              size: e.target.value,
-            })
-          }
-          label="Tamaño"
-          id="formControlDefault"
-          type="text"
-        />
-        <br />
-        <MDBInput
-          tvalue={formState.age}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              age: e.target.value,
-            })
-          }
-          label="Edad"
-          id="formControlDefault"
-          type="text"
-        />
-        <br />
-        <MDBInput
-          value={formState.vaccinationPlan}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              vaccinationPlan: e.target.value,
-            })
-          }
-          label="Plan de vacunación"
-          id="formControlDefault"
-          type="text"
-        />
-        <br />
-        <MDBInput
-          value={formState.careToHave}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              careToHave: e.target.value,
-            })
-          }
-          label="Cuidados a tener"
-          id="formControlDefault"
-          type="text"
-        />
+          <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+              Raza
+            </InputLabel>
+            <NativeSelect
+              value={formState.raceId}
+              onChange={(e) =>
+                setFormState({
+                  ...formState,
+                  raceId: e.target.value,
+                })
+              }
+            >
 
-        <br />
-        <MDBBtn type="submit">Button</MDBBtn>
-      </form>
-      <br />
-    </div>
+              <option value={4}>Mestiza</option>
+              <option value={1}>Chiwawa</option>
+              <option value={2}>Pastor Berga</option>
+            </NativeSelect>
+          </FormControl>
+
+          <br />
+          <br />
+          <MDBInput
+            value={formState.size}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                size: e.target.value,
+              })
+            }
+            label="Tamaño"
+            id="formControlDefault"
+            type="text"
+          />
+          <br />
+          <MDBInput
+            tvalue={formState.age}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                age: e.target.value,
+              })
+            }
+            label="Edad"
+            id="formControlDefault"
+            type="text"
+          />
+          <br />
+          <MDBInput
+            value={formState.vaccinationPlan}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                vaccinationPlan: e.target.value,
+              })
+            }
+            label="Plan de vacunación"
+            id="formControlDefault"
+            type="text"
+          />
+          <br />
+          <MDBInput
+            value={formState.careToHave}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                careToHave: e.target.value,
+              })
+            }
+            label="Cuidados a tener"
+            id="formControlDefault"
+            type="text"
+          />
+
+          <br />
+          <MDBBtn type="submit">Ingresar</MDBBtn>
+        </form>
+      </div>
+      <CompleteFormPet
+          data={formState.petName}
+          gridModal={gridModal}
+          setGridModal={setGridModal}
+        ></CompleteFormPet>
+    </section>
   );
 };
 

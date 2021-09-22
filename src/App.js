216d510c1aa0e911b7921a776e18e37e96aa4689
presi11 <juan.ospina19@udea.edu.co>
2021-09-22 15:53:35 -0500

    material ui

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+/* import { Route, Switch, Redirect } from "react-router-dom"; */
 import NavbarHOC from "./HOC/Layout/NavBar/NavbarHOC";
-import Footer from "./HOC/Layout/Footer/Footer";
+/* import Footer from "./HOC/Layout/Footer/Footer";
 import Register from "./Pages/Pets/RegisterPet";
 import Home from "./Pages/Home/Home";
 import MePets from "./Pages/Pets/MePets";
-import PetAdmin from "./Pages/Admin/Director/PetAdmin";
+import PetAdmin from "./Pages/Admin/Director/PetAdmin"; */
 import "./App.css";
 
-
 const App = () => {
-
   return (
     <NavbarHOC>
-      
-
-      <div className="Body">
-      <Switch>
-        <Redirect exact from="/" to="/Home" />
-        <Route path="/Register" component={Register} />
-        <Route path="/Home" component={Home} />
-        <Route path="/MePets" component={MePets} />
-        <Route path="/PetAdmin" component={PetAdmin}/>
-        <Route path="*" render={() => <p>NO TENGO NADA</p>} />
-      </Switch>
+{/*       <div className="Body">
+        <Switch>
+          <Redirect exact from="/" to="/Home" />
+          <Route path="/Register" component={Register} />
+          <Route path="/Home" component={Home} />
+          <Route path="/MePets" component={MePets} />
+          <Route path="/PetAdmin" component={PetAdmin} />
+          <Route path="*" render={() => <p>NO TENGO NADA</p>} />
+        </Switch>
       </div>
- 
-       <Footer/>
-     
-        
-    </NavbarHOC>
 
-    
-    
+      <Footer /> */}
+    </NavbarHOC>
   );
 };
 
--- src/HOC/Layout/NavBar/NavbarHOC.jsx
+++ src/HOC/Layout/NavBar/NavbarHOC.jsx
@@ -1,101 +1,111 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { GoogleLogout } from 'react-google-login';
-import {
-  MDBNavbar,
-  MDBContainer,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBNavbarToggler,
-  MDBNavbarBrand,
-} from "mdb-react-ui-kit";
+import { GoogleLogout } from "react-google-login";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 import LoginGoogle from "../../../Component/Google-Login/LoginGoogle";
 
-
-
 const NavbarHOC = ({ children }) => {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const [showNavColorSecond, setShowNavColorSecond] = useState(false);
 
-  
   const history = useHistory();
   const redirect = (route) => history.push(`/${route}`);
 
   const logOut = () => {
     localStorage.clear();
     history.push("/Home");
-    window.location.reload(false)
+    window.location.reload(false);
   };
 
   return (
-    <>
-      <header>
-        <MDBNavbar expand="lg" light bgColor="warning">
-          <MDBContainer fluid>
-            <MDBNavbarBrand href="#">Pexshop</MDBNavbarBrand>
-            <MDBNavbarToggler
-              type="button"
-              data-target="#navbarColor02"
-              aria-controls="navbarColor02"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              onClick={() => setShowNavColorSecond(!showNavColorSecond)}
-            >
-              <MDBIcon icon="bars" fas />
-            </MDBNavbarToggler>
-            <div className="collapse navbar-collapse" id="navbarExample01">
-              <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
-                <MDBNavbarItem className="active">
-                  <MDBNavbarLink
-                    aria-current="page"
-                    onClick={() => redirect("Home")}
-                  >
-                    Home
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  {!localStorage.getItem("access_token") ? (
-                    <LoginGoogle> login</LoginGoogle>
-                  ) : null}
-                </MDBNavbarItem>
-
-                <MDBNavbarItem>
-                {localStorage.getItem("access_token") ? (
-                    <GoogleLogout
-                    clientId="103162145817-vq4hiompm6h9k073nihc2a9foeft3e7b.apps.googleusercontent.com"
-                    buttonText="Logout"
-                    onLogoutSuccess={logOut}
-                  >
-                  </GoogleLogout>
-                  ) : null}
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  {localStorage.getItem("access_token") ? (
-                    <MDBNavbarLink onClick={() => redirect("Register")}>
-                      Registro Mascota
-                    </MDBNavbarLink>
-                  ) : null}
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink onClick={() => redirect("PetAdmin")}>
-                    Acerca de
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                {localStorage.getItem("access_token") ? (
-                <MDBNavbarItem>
-                  <MDBNavbarLink onClick={() => redirect("MePets")}>
-                    Mis Mascotas
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                ) : null}
-              </MDBNavbarNav>
+    <Box sx={{ flexGrow: 1 }}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={auth}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={auth ? "Logout" : "Login"}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+          
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
             </div>
-          </MDBContainer>
-        </MDBNavbar>
-      </header>
-      {children}
-    </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
